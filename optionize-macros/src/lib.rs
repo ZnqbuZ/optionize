3dd1f8b31ddf256b0d674705989739b7e5524f67
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2026-05-02 17:16:22 +0200

    darling::Result

--- optionize-macros/src/lib.rs
+++ optionize-macros/src/lib.rs
@@ -5,6 +5,6 @@ mod optionize;
 #[proc_macro_attribute]
 pub fn optionize(args: TokenStream, input: TokenStream) -> TokenStream {
     optionize::proc(args.into(), input.into())
-        .map(Into::into)
-        .unwrap_or_else(|e| e.to_compile_error().into())
+        .unwrap_or_else(|e| e.write_errors())
+        .into()
 }
