14afab2a0fb8e0bf64932d1223f18c0000dbd0dc
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2026-05-03 17:09:48 +0200

    print input when err

--- optionize-macros/src/lib.rs
+++ optionize-macros/src/lib.rs
@@ -4,8 +4,12 @@ mod optionize;
 
 #[proc_macro_attribute]
 pub fn optionized(args: TokenStream, input: TokenStream) -> TokenStream {
-    optionize::proc(args.into(), input.into())
-        .unwrap_or_else(|e| e.write_errors())
+    optionize::proc(args.into(), input.clone().into())
+        .unwrap_or_else(|e| {
+            let mut e = e.write_errors();
+            e.extend(proc_macro2::TokenStream::from(input));
+            e
+        })
         .into()
 }
 
